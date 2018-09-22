6dabdaff628ec7069d01b6e11fb764f77a726b4c
tqlijava <tang960901@gmail.com>
2018-09-22 15:54:27 +0800

    CourtCounter

--- app/src/main/java/example/tql/indi/helloworld/MainActivity.java
+++ app/src/main/java/example/tql/indi/helloworld/MainActivity.java
@@ -1,5 +1,6 @@
 package example.tql.indi.helloworld;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
@@ -23,14 +24,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         Button chkButton = (Button) findViewById(R.id.button);
         chkButton.setOnClickListener(new View.OnClickListener(){
@@ -69,8 +63,9 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.pointCount) {
+            Intent intent = new Intent(this, pointCount.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
---
+++ app/src/main/java/example/tql/indi/helloworld/pointCount.java
@@ -0,0 +1,76 @@
+package example.tql.indi.helloworld;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class pointCount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_point_count);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_point, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.tohello) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void addPoint(int id, int point) {
+        TextView points = (TextView) findViewById(id);
+        int current = Integer.parseInt(points.getText().toString());
+        points.setText(String.valueOf(current + point));
+    }
+
+    public void addPoint(View btn){
+        int id = btn.getId();
+        switch(id){
+            case R.id.a1:
+                addPoint(R.id.pointA, 1);
+                break;
+            case R.id.a2:
+                addPoint(R.id.pointA, 2);
+                break;
+            case R.id.a3:
+                addPoint(R.id.pointA, 3);
+                break;
+            case R.id.b1:
+                addPoint(R.id.pointB, 1);
+                break;
+            case R.id.b2:
+                addPoint(R.id.pointB, 2);
+                break;
+            case R.id.b3:
+                addPoint(R.id.pointB, 3);
+                break;
+        }
+    }
+
+    public void clearPoint(View btn) {
+        TextView pointA = (TextView) findViewById(R.id.pointA);
+        TextView pointB = (TextView) findViewById(R.id.pointB);
+        pointA.setText("0");
+        pointB.setText("0");
+    }
+}
